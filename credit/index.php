f8088dbc5a62c21cb5931fb7e9d024db7bef9dae
AymanDasa <ayman.dasa@gmail.com>
2024-04-04 01:47:24 +0300

    Update index.php
    change Credit Note DateTime

--- credit/index.php
+++ credit/index.php
@@ -4,7 +4,7 @@ $folder_name =  basename(dirname(__FILE__));
 Require_once( "C:\\wow\\password\\config.php"); 
 Require_once("../include/auth.php"); 
 Require_once("../include/config.php");   
-$today = date("Y-m-d H:i:s");  
+$today = date("Y-m-d H:i:s") ;
 $ERROR=0;
 #########################################################################
 #########################################################################
@@ -42,7 +42,8 @@ if($debug){echo "<b>reason :</b>".$reason,"<br>";}
 ###########################  SQL INSERT #################################
 #########################################################################
 #########################################################################
-if($InvoiceID){
+if($InvoiceID){  
+	if($_POST['CreditDate']==""){$CreditDate=date("Y-m-d H:i:s", time());}else{$CreditDate=$_POST['CreditDate']; }
 	if(isset($_POST['VAT_TOTAL'])){$User_Total = floatval($_POST['VAT_TOTAL']);}  
 	$SQL1= "SELECT * FROM `invoice` WHERE  `InvoiceID` ='".$InvoiceID."' AND `Status` > '0' LIMIT 1;";
 	if($debug){echo "<b>SQL1 :</b>".$SQL1,"<br>";} 
@@ -65,6 +66,7 @@ if( $VAT_TOTAL == $User_Total) {
 					`ShipID`,
 					`AgentID`,
 					`InvoiceDate`,
+					`CreditDate`,
 					`reason`, 
 					`MSTOTAL`,
 					`SSTOTAL`,
@@ -76,6 +78,7 @@ if( $VAT_TOTAL == $User_Total) {
 					".$ShipID.",				
 					".$AgentID .",					
 					'".$InvoiceDate."',					
+					'".$CreditDate."',					
 					'".$reason."', 				
 					".$MSTOTAL.",					
 					".$SSTOTAL.",					
@@ -99,21 +102,34 @@ if( $VAT_TOTAL == $User_Total) {
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <title><?php echo ucwords(basename(dirname(__FILE__)));?></title>  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo ucwords(basename(dirname(__FILE__)));?></title>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/fontawesome-free/css/all.min.css"> 
   <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/fontawesome-free6/css/all.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/daterangepicker/daterangepicker.css">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- Bootstrap4 Duallistbox -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+  <!-- BS Stepper -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/bs-stepper/css/bs-stepper.min.css">
+  <!-- dropzonejs -->
+  <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/plugins/dropzone/min/dropzone.min.css">  
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo $Homepath;?>adminlte/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="../include/css/jimp.css">
-   <link rel="icon" type="image/x-icon" href="../include/img/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="../include/img/favicon.ico">
 </head>
 <body class="hold-transition sidebar-mini"> 
 <div class="wrapper">
@@ -291,10 +307,27 @@ if( $VAT_TOTAL == $User_Total) {
 					<label class="col-form-label" for="inputError"> Invoice Number</label>
 					<input type="text" name="InvoiceID" class="form-control is-invalid" id="inputError" autocomplete="off"/>
 				</div>
-				<div class="form-group">
-					<label class="col-form-label" for="inputError">Invoice Total Amount</label>
-					<input type="text" name="VAT_TOTAL" class="form-control is-invalid" id="inputError" autocomplete="off"/>
-				</div> 
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label class="col-form-label" for="inputError">Invoice Total Amount</label>
+							<input type="text" name="VAT_TOTAL" value="8642.25" class="form-control is-invalid" id="inputError" autocomplete="off"/>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label>Credit-Note Date</label>
+							<div class="input-group date" id="CreditDate" data-target-input="nearest">
+								<input  name="CreditDate" type="text" class="form-control datetimepicker-input" data-target="#CreditDate" placeholder="<?php echo $today;?>"/>
+								<div class="input-group-append" data-target="#CreditDate" data-toggle="datetimepicker">
+									<div class="input-group-text">
+										<i class="fa fa-calendar"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div class="form-group">
                         <label>Reason</label>
                         <textarea class="form-control" name="reason" rows="3" placeholder=""></textarea>
@@ -312,29 +345,37 @@ if( $VAT_TOTAL == $User_Total) {
       </div>
 
 
-<!-- ./wrapper --> 
-<script src="<?php echo $Homepath;?>adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?php echo $Homepath;?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/jszip/jszip.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="<?php echo $Homepath;?>adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo $Homepath;?>adminlte/dist/js/adminlte.min.js"></script> 
-<!-- Page specific script -->
+	 <script src="<?php echo $Homepath;?>adminlte/plugins/jquery/jquery.min.js"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Select2 -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/select2/js/select2.full.min.js"></script>
+	<!-- Bootstrap4 Duallistbox -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+	<!-- InputMask -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/moment/moment.min.js"></script>
+	<script src="<?php echo $Homepath;?>adminlte/plugins/inputmask/jquery.inputmask.min.js"></script>
+	<!-- date-range-picker -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/daterangepicker/daterangepicker.js"></script>
+	<!-- bootstrap color picker -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+	<!-- Tempusdominus Bootstrap 4 -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+	<!-- Bootstrap Switch -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+	<!-- BS-Stepper -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+	<!-- dropzonejs -->
+	<script src="<?php echo $Homepath;?>adminlte/plugins/dropzone/min/dropzone.min.js"></script>   
+	<!-- AdminLTE App -->
+	<script src="<?php echo $Homepath;?>adminlte/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script>
   $(function () {
+	$('#CreditDate').datetimepicker({
+    format: 'YYYY-MM-DD HH:mm'
+    }); 
+
     $("#example1").DataTable({ 
 	 "responsive": true, 
 	 "lengthChange": false,  
@@ -347,6 +388,12 @@ if( $VAT_TOTAL == $User_Total) {
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
      
   }); 
+
+ 
+    
+
+
+
 </script>
       
 <script src="<?php echo $path;?>include/js/menu.js"></script>
