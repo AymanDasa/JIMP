c7a16db5a79b1b85ba8a7f720b77ea71dde164d5
AymanDasa <ayman.dasa@gmail.com>
2024-05-05 12:12:15 +0300

    V 4.17.1 Beta

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 4.17.0
+      <b>Version</b> 4.17.1 (beta)
     </div>
     <strong>Copyright &copy; 2024 <a href="https://vexam.net">vExam</a>.</strong> All rights reserved. <?= $_SESSION['expiry_time'];?>
   </footer>
