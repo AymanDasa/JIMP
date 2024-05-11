047104c0f2d149def4751c258824e888ac4236c7
AymanDasa <ayman.dasa@gmail.com>
2024-05-11 10:01:05 +0300

    d

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hi Ayman
+<?php   
+	session_start();  
+	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){ 
+		header("location: landpage.php");
+		exit;
+	} else { 
+		header("location: access/index.php");
+		exit;
+	}
+?> 
